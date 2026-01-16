585dc379a8e83eed9d731b0e4477fe44b2c6281f
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-01-16 12:49:45 +0300

    feat: add footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Screen } from "../interfaces/screen.interface.js";
+import footer from "../layout/footer/footer.js";
 import header from "../layout/header/header.js";
 import { Page } from "../types/page.type.js";
 import MainCreator from "../utils/main/main-creator.js";
@@ -9,9 +10,8 @@ export default class App {
   private root: HTMLElement;
 
   private headerElement?: HTMLElement;
-  private main = new MainCreator({
-    classes: ["main"],
-  }).getElement();
+  private main?: HTMLElement;
+  private footer?: HTMLElement;
 
   constructor(root: HTMLElement) {
     this.root = root;
@@ -19,14 +19,20 @@ export default class App {
 
   init(initPage: Page): void {
     this.headerElement = header(initPage);
-
+    this.main = new MainCreator({
+      classes: ["main"],
+    }).getElement();
+    this.footer = footer();
     this.root.append(this.headerElement);
     this.root.append(this.main);
+    this.root.append(this.footer);
   }
 
   register(page: Page, screen: Screen): void {
     this.pages.set(page, screen);
-    screen.create(this.root);
+    if (this.main) {
+      screen.create(this.main);
+    }
     screen.hide();
   }
 
