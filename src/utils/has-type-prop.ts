e92f44060b9e20ab813fc129aff97a41acdb9105
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-01-27 15:59:22 +0300

    fix: fix type assertion in has-type-prop.ts

--- src/utils/has-type-prop.ts
+++ src/utils/has-type-prop.ts
@@ -1,17 +1,35 @@
-export function hasStringProp(obj: object, key: string): boolean {
+export function hasStringProp(
+  obj: unknown,
+  key: string,
+): obj is Record<string, unknown> & { [K in typeof key]: string } {
   return (
-    key in obj && typeof (obj as Record<string, unknown>)[key] === "string"
+    typeof obj === "object" &&
+    obj !== null &&
+    key in obj &&
+    typeof (obj as Record<string, unknown>)[key] === "string"
   );
 }
 
-export function hasNumberProp(obj: object, key: string): boolean {
+export function hasNumberProp(
+  obj: unknown,
+  key: string,
+): obj is Record<string, unknown> & { [K in typeof key]: number } {
   return (
-    key in obj && typeof (obj as Record<string, unknown>)[key] === "number"
+    typeof obj === "object" &&
+    obj !== null &&
+    key in obj &&
+    typeof (obj as Record<string, unknown>)[key] === "number"
   );
 }
 
-export function hasBooleanProp(obj: object, key: string): boolean {
+export function hasBooleanProp(
+  obj: unknown,
+  key: string,
+): obj is Record<string, unknown> & { [K in typeof key]: boolean } {
   return (
-    key in obj && typeof (obj as Record<string, unknown>)[key] === "boolean"
+    typeof obj === "object" &&
+    obj !== null &&
+    key in obj &&
+    typeof (obj as Record<string, unknown>)[key] === "boolean"
   );
 }
