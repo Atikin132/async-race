3b08870aa660bba3614c00cb8a135be4236b1042
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-07-31 15:25:59 +0300

    feat: add gitignore and README

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/atikin132-JSFE2025Q3/async-race/",
+  base: "/async-race/",
   server: {
     port: 8080,
     open: true,
